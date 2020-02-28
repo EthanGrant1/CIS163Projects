f5f0d5bf98ff83756274b06f16c2b5e1e94615bd
bull4098 <59898759+bull4098@users.noreply.github.com>
2020-02-28 13:00:19 -0500

    Add files via upload

--- Project3/RV.java
+++ Project3/RV.java
@@ -2,10 +2,11 @@ package Project3;
 
 import java.util.Calendar;
 import java.util.GregorianCalendar;
+import java.time.temporal.ChronoUnit;
 
 public class RV extends CampSite {
 
-private int power;
+    private int power;
 
     public RV() {
     }
@@ -13,7 +14,7 @@ private int power;
     public RV(String guestName, GregorianCalendar checkIn, GregorianCalendar estimatedCheckOut, GregorianCalendar actualCheckOut, int power) {
         super(guestName, checkIn, estimatedCheckOut, actualCheckOut);
         this.power = power;
-        }
+    }
 
     public int getPower() {
         return power;
@@ -26,224 +27,18 @@ private int power;
     @Override
     public double getCost(GregorianCalendar checkOut) {
         // Initial cost of 10
-        double cost = 10.0;
+        double cost = 10;
 
-        // If the number of watts is less or equal to 1000
-        // and the checkIn / checkOut years are the same
-        if (this.getPower() <= 1000 &&
-                this.checkIn.get(Calendar.YEAR) == checkOut.get(Calendar.YEAR)) {
+        GregorianCalendar gTemp = new GregorianCalendar();
+        gTemp = (GregorianCalendar) checkOut.clone();
 
-            // For loop to add $20 to the cost for every day
-            // the RV is at the camp site
-            for (int i = this.checkIn.get(Calendar.DAY_OF_YEAR);
-                 i < checkOut.get(Calendar.DAY_OF_YEAR); i++) {
-
-                cost += 20;
-
-            }
-        }
-
-        // If the number of watts is greater than 1000
-        // and the checkIn / checkOut years are the same
-        if (this.getPower() > 1000 &&
-                this.checkIn.get(Calendar.YEAR) == checkOut.get(Calendar.YEAR)) {
-
-            // For loop to add $30 to the cost for every day
-            // the RV is at the camp site
-            for (int i = this.checkIn.get(Calendar.DAY_OF_YEAR);
-                 i < checkOut.get(Calendar.DAY_OF_YEAR); i++) {
-
+        while(gTemp.after(checkIn)){
+            if(power > 1000)
                 cost += 30;
-
-            }
-
-        }
-
-        // If the number of watts is less than or equal to 1000,
-        // the checkIn / checkOut years are not the same,
-        // and the checkIn year does not need to immediately
-        // roll over to the next year
-        if (this.getPower() <= 1000 &&
-                (this.checkIn.get(Calendar.YEAR) < checkOut.get(Calendar.YEAR)) &&
-                (this.checkIn.get(Calendar.YEAR) != checkOut.get(Calendar.YEAR) - 1)) {
-
-            // For loop to progress the number of years until it
-            // matches the checkOut year
-            for (int i = this.checkIn.get(Calendar.YEAR);
-                 i < checkOut.get(Calendar.YEAR); i++) {
-
-                // Adds a year's worth of money to the
-                // cost depending on if the current year
-                // is a leap year or not
-                if (!this.checkIn.isLeapYear(i)) {
-                    cost += 365*20;
-                }
-
-                if (this.checkIn.isLeapYear(i)) {
-                    cost += 366*20;
-                }
-            }
-
-            // For loop to add the rest of the money to the cost
-            for (int j = this.checkIn.get(Calendar.DAY_OF_YEAR);
-                 j < checkOut.get(Calendar.DAY_OF_YEAR); j++) {
-
+            else
                 cost += 20;
-            }
-
+            gTemp.add(Calendar.DATE, -1);
         }
-
-        // If the number of watts is greater than 1000,
-        // the checkIn / checkOut years are not the same,
-        // and the checkIn year does not need to immediately
-        // roll over to the next year
-        if (this.getPower() > 1000 &&
-                (this.checkIn.get(Calendar.YEAR) < checkOut.get(Calendar.YEAR)) &&
-                (this.checkIn.get(Calendar.YEAR) != checkOut.get(Calendar.YEAR) - 1)) {
-
-            // For loop to progress the number of years until it
-            // matches the checkOut year
-            for (int i = this.checkIn.get(Calendar.YEAR);
-                 i < checkOut.get(Calendar.YEAR); i++) {
-
-                // Adds a year's worth of money to the
-                // cost depending on if the current year
-                // is a leap year or not
-                if (!this.checkIn.isLeapYear(i)) {
-                    cost += 365*30;
-                }
-
-                if (this.checkIn.isLeapYear(i)) {
-                    cost += 366*30;
-                }
-            }
-
-            // For loop to add the rest of the money to the cost
-            for(int j = this.checkIn.get(Calendar.DAY_OF_YEAR);
-                j < checkOut.get(Calendar.DAY_OF_YEAR); j++) {
-
-                cost += 30;
-            }
-
-        }
-
-        // If the number of watts is less than or equal to 1000,
-        // the checkIn / checkOut years are not the same,
-        // and the checkIn year needs to immediately
-        // roll over to the next year
-        if (this.getPower() <= 1000 &&
-                this.checkIn.get(Calendar.YEAR) == checkOut.get(Calendar.YEAR) - 1) {
-
-            // For loop to progress the number of years until it
-            // matches the checkOut year
-            for (int i = this.checkIn.get(Calendar.YEAR);
-                 i <= checkOut.get(Calendar.YEAR); i++) {
-
-                // If the current year is less than the checkOut
-                // year
-                if (i < checkOut.get(Calendar.YEAR)) {
-
-                    // If the current year is not a leap year
-                    if (!this.checkIn.isLeapYear(i)) {
-
-                        // For loop to add money to the cost
-                        // up to the final day of the year
-                        for (int j = this.checkIn.get(Calendar.DAY_OF_YEAR);
-                             j <= 365; j++) {
-
-                            cost += 20;
-                        }
-
-                    }
-
-                    // If the current year is a leap year
-                    if (this.checkIn.isLeapYear(i)) {
-
-                        // For loop to add money to the cost
-                        // up to the final day of the year
-                        for (int j = this.checkIn.get(Calendar.DAY_OF_YEAR);
-                             j <= 366; j++) {
-
-                            cost += 20;
-                        }
-
-                    }
-                }
-
-                // If the checkOut year is equal to the current year
-                if (i == checkOut.get(Calendar.YEAR)) {
-
-                    // For loop to add the rest of the money to the cost
-                    // until the checkOut date
-                    for (int j = 1;
-                         j < checkOut.get(Calendar.DAY_OF_YEAR); j++) {
-
-                        cost += 20;
-                    }
-                }
-            }
-
-        }
-
-        // If the number of watts is greater than 1000,
-        // the checkIn / checkOut years are not the same,
-        // and the checkIn year needs to immediately
-        // roll over to the next year
-        if (this.getPower() > 1000 &&
-                this.checkIn.get(Calendar.YEAR) == checkOut.get(Calendar.YEAR) - 1) {
-
-            // For loop to progress the number of years until it
-            // matches the checkOut year
-            for (int i = this.checkIn.get(Calendar.YEAR);
-                 i <= checkOut.get(Calendar.YEAR); i++) {
-
-                // If the current year is less than
-                // the checkOut year
-                if (i < checkOut.get(Calendar.YEAR)) {
-
-                    // If the current year is not a leap year
-                    if (!checkOut.isLeapYear(i)) {
-
-                        // For loop to add money to the cost
-                        // up to the final day of the year
-                        for (int j = this.checkIn.get(Calendar.DAY_OF_YEAR);
-                             j <= 365; j++) {
-
-                            cost += 30;
-                        }
-
-                    }
-
-                    // If the current year is a leap year
-                    if (checkOut.isLeapYear(i)) {
-
-                        // For loop to add money to the cost
-                        // up to the final day of the year
-                        for (int j = this.checkIn.get(Calendar.DAY_OF_YEAR);
-                             j <= 366; j++) {
-
-                            cost += 30;
-                        }
-
-                    }
-                }
-
-                // If the current year is equal to the
-                // checkOut year
-                if (i == checkOut.get(Calendar.YEAR)) {
-
-                    // For loop to add the rest of the money to the cost
-                    // until the checkOut date
-                    for (int j = 1;
-                         j < checkOut.get(Calendar.DAY_OF_YEAR); j++) {
-
-                        cost += 30;
-                    }
-                }
-            }
-
-        }
-
         return cost;
     }
 
--- Project3/TentOnly.java
+++ Project3/TentOnly.java
@@ -2,6 +2,7 @@ package Project3;
 
 import java.util.Calendar;
 import java.util.GregorianCalendar;
+import java.time.temporal.ChronoUnit;
 
 public class TentOnly extends CampSite {
 
@@ -33,222 +34,16 @@ public class TentOnly extends CampSite {
         // Initial cost of 0
         double cost = 0.0;
 
-        // If the number of Tenters is less than 10
-        // and the checkIn / checkOut years are the same
-        if (this.getNumberOfTenters() < 10 &&
-                this.checkIn.get(Calendar.YEAR) == checkOut.get(Calendar.YEAR)) {
+        GregorianCalendar gTemp = new GregorianCalendar();
+        gTemp = (GregorianCalendar) checkOut.clone();
 
-                // For loop to add $10 to the cost for every day
-                // the Tenters are at the camp site
-                for (int i = this.checkIn.get(Calendar.DAY_OF_YEAR);
-                     i < checkOut.get(Calendar.DAY_OF_YEAR); i++) {
-
-                    cost += 10;
-
-                }
-        }
-
-        // If the number of Tenters is greater than 10
-        // and the checkIn / checkOut years are the same
-        if (this.getNumberOfTenters() > 10 &&
-                this.checkIn.get(Calendar.YEAR) == checkOut.get(Calendar.YEAR)) {
-
-                // For loop to add $20 to the cost for every day
-                // the Tenters are at the camp site
-                for (int i = this.checkIn.get(Calendar.DAY_OF_YEAR);
-                     i < checkOut.get(Calendar.DAY_OF_YEAR); i++) {
-
-                    cost += 20;
-
-                }
-
-        }
-
-        // If the number of Tenters is less than 10,
-        // the checkIn / checkOut years are not the same,
-        // and the checkIn year does not need to immediately
-        // roll over to the next year
-        if (this.getNumberOfTenters() < 10 &&
-                (this.checkIn.get(Calendar.YEAR) < checkOut.get(Calendar.YEAR)) &&
-                (this.checkIn.get(Calendar.YEAR) != checkOut.get(Calendar.YEAR) - 1)) {
-
-            // For loop to progress the number of years until it
-            // matches the checkOut year
-            for (int i = this.checkIn.get(Calendar.YEAR);
-                 i < checkOut.get(Calendar.YEAR); i++) {
-
-                // Adds a year's worth of money to the
-                // cost depending on if the current year
-                // is a leap year or not
-                if (!this.checkIn.isLeapYear(i)) {
-                    cost += 365*10;
-                }
-
-                if (this.checkIn.isLeapYear(i)) {
-                    cost += 366*10;
-                }
-            }
-
-            // For loop to add the rest of the money to the cost
-            for (int j = this.checkIn.get(Calendar.DAY_OF_YEAR);
-                j < checkOut.get(Calendar.DAY_OF_YEAR); j++) {
-
-                cost += 10;
-            }
-
-        }
-
-        // If the number of Tenters is greater than 10,
-        // the checkIn / checkOut years are not the same,
-        // and the checkIn year does not need to immediately
-        // roll over to the next year
-        if (this.getNumberOfTenters() > 10 &&
-                (this.checkIn.get(Calendar.YEAR) < checkOut.get(Calendar.YEAR)) &&
-                (this.checkIn.get(Calendar.YEAR) != checkOut.get(Calendar.YEAR) - 1)) {
-
-            // For loop to progress the number of years until it
-            // matches the checkOut year
-            for (int i = this.checkIn.get(Calendar.YEAR);
-                 i < checkOut.get(Calendar.YEAR); i++) {
-
-                // Adds a year's worth of money to the
-                // cost depending on if the current year
-                // is a leap year or not
-                if (!this.checkIn.isLeapYear(i)) {
-                    cost += 365*20;
-                }
-
-                if (this.checkIn.isLeapYear(i)) {
-                    cost += 366*20;
-                }
-            }
-
-            // For loop to add the rest of the money to the cost
-            for(int j = this.checkIn.get(Calendar.DAY_OF_YEAR);
-                j < checkOut.get(Calendar.DAY_OF_YEAR); j++) {
-
+        while(gTemp.after(checkIn)){
+            if(numberOfTenters > 10)
                 cost += 20;
-            }
-
+            else
+                cost += 10;
+            gTemp.add(Calendar.DATE, -1);
         }
-
-        // If the number of Tenters is less than 10,
-        // the checkIn / checkOut years are not the same,
-        // and the checkIn year needs to immediately
-        // roll over to the next year
-        if (this.getNumberOfTenters() < 10 &&
-                this.checkIn.get(Calendar.YEAR) == checkOut.get(Calendar.YEAR) - 1) {
-
-            // For loop to progress the number of years until it
-            // matches the checkOut year
-            for (int i = this.checkIn.get(Calendar.YEAR);
-                 i <= checkOut.get(Calendar.YEAR); i++) {
-
-                // If the current year is less than the checkOut
-                // year
-                if (i < checkOut.get(Calendar.YEAR)) {
-
-                    // If the current year is not a leap year
-                    if (!this.checkIn.isLeapYear(i)) {
-
-                        // For loop to add money to the cost
-                        // up to the final day of the year
-                        for (int j = this.checkIn.get(Calendar.DAY_OF_YEAR);
-                        j <= 365; j++) {
-
-                            cost += 10;
-                        }
-
-                    }
-
-                    // If the current year is a leap year
-                    if (this.checkIn.isLeapYear(i)) {
-
-                        // For loop to add money to the cost
-                        // up to the final day of the year
-                        for (int j = this.checkIn.get(Calendar.DAY_OF_YEAR);
-                             j <= 366; j++) {
-
-                            cost += 10;
-                        }
-
-                    }
-                }
-
-                // If the checkOut year is equal to the current year
-                if (i == checkOut.get(Calendar.YEAR)) {
-
-                    // For loop to add the rest of the money to the cost
-                    // until the checkOut date
-                    for (int j = 1;
-                         j < checkOut.get(Calendar.DAY_OF_YEAR); j++) {
-
-                        cost += 10;
-                    }
-                }
-            }
-
-        }
-
-        // If the number of Tenters is greater than 10,
-        // the checkIn / checkOut years are not the same,
-        // and the checkIn year needs to immediately
-        // roll over to the next year
-        if (this.getNumberOfTenters() > 10 &&
-                this.checkIn.get(Calendar.YEAR) == checkOut.get(Calendar.YEAR) - 1) {
-
-            // For loop to progress the number of years until it
-            // matches the checkOut year
-            for (int i = this.checkIn.get(Calendar.YEAR);
-                 i <= checkOut.get(Calendar.YEAR); i++) {
-
-                // If the current year is less than
-                // the checkOut year
-                if (i < checkOut.get(Calendar.YEAR)) {
-
-                    // If the current year is not a leap year
-                    if (!checkOut.isLeapYear(i)) {
-
-                        // For loop to add money to the cost
-                        // up to the final day of the year
-                        for (int j = this.checkIn.get(Calendar.DAY_OF_YEAR);
-                             j <= 365; j++) {
-
-                            cost += 20;
-                        }
-
-                    }
-
-                    // If the current year is a leap year
-                    if (checkOut.isLeapYear(i)) {
-
-                        // For loop to add money to the cost
-                        // up to the final day of the year
-                        for (int j = this.checkIn.get(Calendar.DAY_OF_YEAR);
-                             j <= 366; j++) {
-
-                            cost += 20;
-                        }
-
-                    }
-                }
-
-                // If the current year is equal to the
-                // checkOut year
-                if (i == checkOut.get(Calendar.YEAR)) {
-
-                    // For loop to add the rest of the money to the cost
-                    // until the checkOut date
-                    for (int j = 1;
-                         j < checkOut.get(Calendar.DAY_OF_YEAR); j++) {
-
-                        cost += 20;
-                    }
-                }
-            }
-
-        }
-
         return cost;
     }
 
