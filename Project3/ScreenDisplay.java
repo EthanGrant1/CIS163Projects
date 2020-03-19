1d6abaa5f3426c8b5572bddbc593e7041386c211
EthanGrant1 <59736479+EthanGrant1@users.noreply.github.com>
2020-03-19 15:32:35 -0400

    Add files via upload

---
+++ Project3/ScreenDisplay.java
@@ -0,0 +1,11 @@
+package Project3;
+
+public enum ScreenDisplay {
+    CurrentParkStatus,
+    CheckOutGuest,
+    OverDueScreen,
+    SortByRVTent,
+    SortByTentRV,
+    ThisWillThrowAnError,
+    UpdateScreenErrorBypass,
+}
